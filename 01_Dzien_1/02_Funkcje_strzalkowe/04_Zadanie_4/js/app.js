6efb4301fe40106329ba79c198c0208601883b66
Kamil Szczepaniec <kamil.szczepaniec@wp.pl>
2022-03-02 14:22:13 +0100

    fat arrow in prototypes

--- 01_Dzien_1/02_Funkcje_strzalkowe/04_Zadanie_4/js/app.js
+++ 01_Dzien_1/02_Funkcje_strzalkowe/04_Zadanie_4/js/app.js
@@ -1,14 +1,30 @@
 const App = function () {
   this.websites = ["google", "twitter", "facebook"];
   this.links = [];
+
 };
 
 App.prototype.generateLinks = function () {
-
+  this.links.push(this.websites.map(el => `https://${el}.com`));
 };
 
 App.prototype.insertLinks = function () {
+  let list = document.querySelector('#menu');
+  console.log(this.links);
+    // let arr = [...this.websites];
 
+  this.websites.forEach(el => {
+    let liEl = document.createElement('li');
+    let aEl = document.createElement('a');
+
+    aEl.setAttribute('href', `https://${el}.com`);
+    liEl.innerText = `https://${el}.com`;
+
+    liEl.append(aEl);
+
+    list.append(liEl);
+  });
+  
 };
 
 const app = new App();
