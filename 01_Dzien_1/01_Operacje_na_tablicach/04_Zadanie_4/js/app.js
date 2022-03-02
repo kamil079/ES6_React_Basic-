43dc0df6dbf2284c5c3fb952cc312a8e9350adc7
Kamil Szczepaniec <kamil.szczepaniec@wp.pl>
2022-03-02 11:57:19 +0100

    add el to ul with content

--- 01_Dzien_1/01_Operacje_na_tablicach/04_Zadanie_4/js/app.js
+++ 01_Dzien_1/01_Operacje_na_tablicach/04_Zadanie_4/js/app.js
@@ -3,49 +3,77 @@ const products = [
     name: "Fiat Tipo",
     category: "car",
     price: 29900,
-    condition: "new"
+    condition: "new",
   },
   {
     name: "MacBook Pro",
     category: "computer",
     price: 9999,
-    condition: "new"
+    condition: "new",
   },
   {
     name: "Xiaomi Redmi Note 8",
     category: "phone",
     price: 2399,
-    condition: "new"
+    condition: "new",
   },
   {
     name: "Audi A6",
     category: "car",
     price: 23450,
-    condition: "used"
+    condition: "used",
   },
   {
     name: "Samsung Galaxy Note 9",
     category: "phone",
     price: 1899,
-    condition: "used"
+    condition: "used",
   },
   {
     name: "Seat Leon",
     category: "car",
     price: 66000,
-    condition: "new"
+    condition: "new",
   },
   {
     name: "Nike Sneakers",
     category: "shoes",
     price: 345,
-    condition: "new"
+    condition: "new",
   },
   {
     name: "Dacia Logan",
     category: "car",
     price: 33500,
-    condition: "new"
-  }
+    condition: "new",
+  },
 ];
 
+let allCars = document.querySelector("#all-products");
+let filteredCars = document.querySelector("#filtered-products");
+
+products.forEach((el) => {
+  let oneEl = document.createElement("li");
+  let nameEl = document.createElement("strong");
+  let infoEl = document.createElement("span");
+
+  nameEl.innerText = `${el.name}`;
+  infoEl.innerText = `${el.price} - ${el.condition}`;
+
+  oneEl.append(nameEl);
+  allCars.append(oneEl);
+});
+
+products.filter((el) => {
+  let oneEl = document.createElement("li");
+  let nameEl = document.createElement("strong");
+  let infoEl = document.createElement("span");
+
+  if (el.category == "car" && el.condition == "new" && el.price < 45000) {
+    nameEl.innerText = `${el.name}`;
+    infoEl.innerText = `${el.price} - ${el.condition}`;
+
+    oneEl.append(nameEl);
+    filteredCars.append(oneEl);
+  }
+});
