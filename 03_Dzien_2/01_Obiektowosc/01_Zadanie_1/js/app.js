f78da8a88fe4bb02a3dc89c079f629220277c693
kamil079 <kamil.szczepaniec@wp.pl>
2022-03-05 13:12:10 +0100

    class extend

--- 03_Dzien_2/01_Obiektowosc/01_Zadanie_1/js/app.js
+++ 03_Dzien_2/01_Obiektowosc/01_Zadanie_1/js/app.js
@@ -1,21 +1,50 @@
 class Vehicle {
-    constructor(name, move) {
+    constructor(name) {
         this.name = name;
-        this.move = move;
     }
     getName() {
         return this.name;
     }
 }
 
-class Boat extends Vehicle {}
-class Car extends Vehicle {}
-class Plane extends Vehicle {}
+
+class Boat extends Vehicle {
+    constructor(name, move) {
+        super(name);
+        this.move = move;
+    }
+    getMoving() {
+        return super.getName() + " " + this.move;
+    }
+}
+
+class Car extends Vehicle {
+    constructor(name, move) {
+        super(name);
+        this.move = move;
+    }
+    getMoving() {
+        return super.getName() + " " + this.move;
+    }
+}
+
+class Plane extends Vehicle {
+    constructor(name, move) {
+        super(name);
+        this.move = move;
+    }
+    getMoving() {
+        return super.getName() + " " + this.move;
+    }
+}
 
 let boat = new Boat("jetski", "swim");
 let car = new Car("BMW", "drive");
 let plane = new Plane("F-16", "fly");
 
 console.log(boat);
+console.log(boat.getMoving());
 console.log(car);
+console.log(car.getMoving());
 console.log(plane);
+console.log(plane.getMoving());
