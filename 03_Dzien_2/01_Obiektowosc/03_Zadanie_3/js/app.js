97628e557f85d217e66ee9317f676bf19020f4b8
kamil079 <kamil.szczepaniec@wp.pl>
2022-03-05 17:22:23 +0100

    to be cdn

--- 03_Dzien_2/01_Obiektowosc/03_Zadanie_3/js/app.js
+++ 03_Dzien_2/01_Obiektowosc/03_Zadanie_3/js/app.js
@@ -1,10 +1,60 @@
 const foods = [];
 
 class Food {
-  constructor(name, protein, carbs, fat) {
-    this.name = name;
-    this.protein = protein;
-    this.carbs = carbs;
-    this.fat = fat;
-  }
+    constructor(name, protein, carbs, fat) {
+        this.name = name;
+        this.protein = protein;
+        this.carbs = carbs;
+        this.fat = fat;
+    }
+    calories() {
+        let totalCalories = this.protein * 4 + this.carbs * 4 + this.fat * 9;
+        console.log(totalCalories);
+    }
 }
+
+class FatFreeFood extends Food {
+    constructor(name, protein, carbs, fat) {
+        super(name, protein, carbs, fat);
+    }
+}
+class FastFood extends Food {
+    constructor(name, protein, carbs, fat) {
+        super(name, protein, carbs, fat);
+    }
+}
+
+let productList = document.querySelector("#products");
+let foodNameEl = document.querySelector("#name");
+let quanProtein = document.querySelector("#proteins");
+let quanCarbs = document.querySelector("#carbs");
+let quanFats = document.querySelector("#fat");
+let btn = document.querySelector(".btn");
+
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let foodName = foodNameEl.value;
+    let res = parseInt(
+        quanProtein.value * 4 + quanCarbs.value * 4 + quanFats.value * 9
+    );
+    console.log();
+    if (res < 250) {
+        foodName = new FatFreeFood(
+            foodName,
+            quanProtein.value,
+            quanCarbs.value,
+            quanFats.value
+        );
+    } else {
+        foodName = new FastFood(
+            foodName,
+            quanProtein.value,
+            quanCarbs.value,
+            quanFats.value
+        );
+    }
+
+    foods.push(foodName);
+    console.log(foods);
+});
