a062e72e7dfea286c16d65c055aac0eb2751b7a8
kamil079 <kamil.szczepaniec@wp.pl>
2022-03-05 16:17:38 +0100

    class obj

--- 03_Dzien_2/01_Obiektowosc/02_Zadanie_2/js/app.js
+++ 03_Dzien_2/01_Obiektowosc/02_Zadanie_2/js/app.js
@@ -3,17 +3,68 @@ class Duck {
         this.type = type;
     }
     sound() {
-        console.log('Quack quack!')
+        console.log("Quack quack!");
     }
     swim() {
-        console.log("I'm swimming..")
+        console.log("I'm swimming..");
     }
     print() {
-        console.log(`Looks like ${this.type}!`)
+        console.log(`Looks like ${this.type}!`);
+    }
+    fly() {
+        console.log("I'm flying...");
     }
 }
 
-let donaldDuck = new Duck('ordinary duck');
+let donaldDuck = new Duck("ordinary duck");
 donaldDuck.sound();
 donaldDuck.swim();
 donaldDuck.print();
+
+class WildDuck extends Duck {
+    constructor(type) {
+        super(type);
+    }
+}
+
+let daffyDuck = new WildDuck("wild duck");
+console.log(daffyDuck);
+daffyDuck.print();
+daffyDuck.sound();
+daffyDuck.swim();
+
+class MallarDuck extends Duck {
+    constructor(type) {
+        super(type);
+    }
+}
+
+let daisyDuck = new MallarDuck("mallard");
+console.log(daisyDuck);
+daisyDuck.print();
+daisyDuck.sound();
+daisyDuck.swim();
+
+console.log(donaldDuck);
+donaldDuck.fly();
+console.log(daffyDuck);
+daffyDuck.fly();
+console.log(daisyDuck);
+daisyDuck.fly();
+
+class RubberDuck extends Duck {
+    constructor(type) {
+        super(type);
+    }
+    fly() {
+        console.log("Rubber duck can't fly!");
+    }
+}
+
+let howardTheDuck = new RubberDuck('rubber');
+console.log(howardTheDuck);
+howardTheDuck.fly();
+howardTheDuck.print();
+howardTheDuck.sound();
+howardTheDuck.swim();
+
