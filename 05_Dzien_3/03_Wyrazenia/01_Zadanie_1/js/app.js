a6e93044ec2e6fe21a3b057b9ec1ff2f9b74ef2d
kamil079 <kamil.szczepaniec@wp.pl>
2022-03-11 21:58:22 +0100

    prompt

--- 05_Dzien_3/03_Wyrazenia/01_Zadanie_1/js/app.js
+++ 05_Dzien_3/03_Wyrazenia/01_Zadanie_1/js/app.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+let firstNr = prompt('Podaj pierwszą liczbę:');
+let secondNr = prompt('Podaj drugą liczbę:');
+
+
 ReactDOM.render(
-  <h1>Hello, World!</h1>,
+  <h1>Hello, World! This is from summary of prompt values - {parseFloat(firstNr + secondNr)}</h1>,
   document.getElementById("app")
 );
