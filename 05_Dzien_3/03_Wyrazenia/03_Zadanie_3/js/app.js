c5f136bbe975d13afc5d214b081421637122c272
kamil079 <kamil.szczepaniec@wp.pl>
2022-03-15 22:50:45 +0100

    cd

--- 05_Dzien_3/03_Wyrazenia/03_Zadanie_3/js/app.js
+++ 05_Dzien_3/03_Wyrazenia/03_Zadanie_3/js/app.js
@@ -1,5 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import person from "./data/person";
+
+function objLi() {
+  let ulEl = document.createElement('ul');
+  let liEl = document.createElement('li');
+  for(let el in person) {
+      liEl.innerText = el
+  }
+  ulEl.append(liEl);
+  
+}
 
 ReactDOM.render(
   <h1>Hello, World!</h1>,
