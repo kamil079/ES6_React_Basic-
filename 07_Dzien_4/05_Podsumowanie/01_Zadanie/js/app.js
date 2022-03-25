e46e2eaa34dda305eb448d35f8a742f047f1caa1
kamil079 <kamil.szczepaniec@wp.pl>
2022-03-25 23:07:48 +0100

    calculator

--- 07_Dzien_4/05_Podsumowanie/01_Zadanie/js/app.js
+++ 07_Dzien_4/05_Podsumowanie/01_Zadanie/js/app.js
@@ -1,5 +1,81 @@
-import React, {Component} from "react";
-import ReactDOM from "react-dom";
+import React from "react";
+import items from "./data/products";
+
+let ReceiptHeader = () => {
+    return (
+        <thead>
+            <tr>
+                <td>Nazwa</td>
+                <td>Cena</td>
+                <td>Ilość</td>
+                <td>Łączna cena</td>
+            </tr>
+        </thead>
+    );
+};
+
+let ReceiptItems = ({ items }) => {
+    return (
+        <tbody>
+            <tr>
+                {items.map((el) => {
+                    return (
+                        <ReceiptItem
+                            key={el.code}
+                            name={el.name}
+                            single_price={el.single_price}
+                            qty={el.qty}
+                        />
+                    );
+                })}
+            </tr>
+        </tbody>
+    );
+};
+
+let ReceiptItem = ({ name, single_price, qty }) => {
+    return (
+        <tr>
+            <td>{name}</td>
+            <td>{single_price}</td>
+            <td>{qty}</td>
+            <td>{(single_price * qty).toFixed(2)} zł</td>
+        </tr>
+    );
+};
+
+let price = items.map((el) => el.single_price);
+let quantity = items.map((el) => el.qty);
+let sumArr = price.map(function (el, i) {
+    return el * quantity[i];
+});
+let res = sumArr.reduce((p,c) => p + c);
+console.log(res);
 
 
-ReactDOM.render(<App/>, document.getElementById("app"));
+let ReceiptFooter = () => {
+    return (
+        <tfooter>
+            <tr>
+                <td>RAZEM</td>
+                <td>-</td>
+                <td>-</td>
+                <td>{res}</td>
+            </tr>
+        </tfooter>
+    );
+};
+
+let Receipt = ({ items }) => {
+    return (
+        <table className="table">
+            <ReceiptHeader />
+            <ReceiptItems items={items} />
+            <ReceiptFooter items={items} />
+        </table>
+    );
+};
+
+export default function App() {
+    return <Receipt items={items} />;
+}
