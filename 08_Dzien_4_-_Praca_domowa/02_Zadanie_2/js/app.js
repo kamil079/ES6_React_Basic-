5e22c7d905865254176f1ae548375b88c07144ee
kamil079 <kamil.szczepaniec@wp.pl>
2022-03-28 16:11:31 +0200

    props

--- 08_Dzien_4_-_Praca_domowa/02_Zadanie_2/js/app.js
+++ 08_Dzien_4_-_Praca_domowa/02_Zadanie_2/js/app.js
@@ -1,7 +1,16 @@
-import React, {Component} from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import person from './data/person';
 
-ReactDOM.render(
-  <h1>Hello, World!</h1>,
-  document.getElementById("app")
-);
+let PersonBox = ({props}) => {
+    return (
+        <>
+            <h1>{person.name} {person.surname}</h1>
+            <h2>{person.jobTitle}</h2>
+            <p>{person.phone}</p>
+        </>
+    )
+}
+
+export default function App() {
+    return <PersonBox name={person.name} surname={person.surname} jobTitle={person.jobTitle} phone={person.phone}/>
+}
